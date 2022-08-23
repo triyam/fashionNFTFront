fa3cdbb978391bae8f83cb4fc5960cc29d1ebb5a
Kaustav Saha <64833209+kaustavsaha018@users.noreply.github.com>
2022-08-23 22:32:28 +0530

    landing page building...

---
+++ src/components/LandingPage/Spotlight.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Spotlight = () => {
+  return (
+      <>
+    <div>
+      <h2>Spotlight. Projects you'll love</h2>  
+    </div>
+    <div>
+        CAROUSEL SECTION 
+    </div>
+      </>
+  )
+}
+
+export default Spotlight
